bc65e1c5b01f9248a17d82a14eeb3c9f806ff26d
ImZirix <xxkillerxx.xg@gmail.com>
2025-02-03 16:15:45 +0300

    Added a package to load the site favicons

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -42,5 +43,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new FaviconsWebpackPlugin("./src/assets/images/icons/favicon-16x16.png"),
   ],
 };
