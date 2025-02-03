dcd564d1ac9865245a64be452cdaa346ff13e08a
ImZirix <xxkillerxx.xg@gmail.com>
2025-02-03 18:30:49 +0300

    resetting gh-pages branch due to a problem with the files

--- README.md
+++
@@ -1,2 +0,0 @@
-# weather-app
-A simple weather website that shows real-time weather updates and forecasts for any location.
--- src/modules/TommorowWeather.js
+++
@@ -1,8 +0,0 @@
-export class TommorowWeather {
-  constructor(address, condition, temp, humidity) {
-    this.address = address;
-    this.condition = condition;
-    this.temp = temp;
-    this.humidity = humidity;
-  }
-}
